96a3a7e706c650b44ebb34cc43a7569b11ef14d9
bhardwajsaurav <saurav.bhardwaj@ekaleido.co>
2024-12-24 20:29:21 +0530

    update ui

--- index.js
+++ index.js
@@ -34,12 +34,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Serve the main HTML file
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+// app.get("/", (req, res) => {
+ 
+// });
 
 // Route to handle photo uploads
-app.post("/upload", upload.single("photo"), (req, res) => {
+app.post("/", upload.single("photo"), (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded." });
   }
