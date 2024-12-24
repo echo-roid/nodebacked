5c3d439b8e10efad1806a8f7785229913c347494
bhardwajsaurav <saurav.bhardwaj@ekaleido.co>
2024-12-24 23:08:28 +0530

    update ui

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 // Create Express app
 const app = express();
 app.use(cors({
-  origin: ["https://nodebacked.vercel.app/upload"], // Update with your frontend URL
+  origin: ["https://nodebacked.vercel.app"], // Update with your frontend URL
   methods: ["GET", "POST"]
 }));
 
@@ -35,13 +35,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Serve the main HTML file
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-  res.send("Hello, World!"); 
- });
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+//   res.send("Hello, World!"); 
+//  });
 
 // Route to handle photo uploads
-app.post("/upload", upload.single("photo"), (req, res) => {
+app.post("/", upload.single("photo"), (req, res) => {
   res.send("Hello, file!"); 
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded." });
